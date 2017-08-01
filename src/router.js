5a7dc61893b58543dfb8295151e12f4d373ea308
samer elaila <samer-elaila@live.com>
2017-08-01 14:49:40 +0300

    add package.json file and other files Relates #10

---
+++ src/router.js
@@ -0,0 +1,17 @@
+var handlers = require('./handlers.js');
+
+function router(req, res){
+  var url = req.url;
+  // handle Home
+  if(url === "/"){
+    handlers.handleHome(req, res);
+  }
+  // handle Find Words
+  else if(url === "/find-words"){
+    hanlers.handleWords(req, res);
+  }
+  
+}
+
+
+module.exports = router;
