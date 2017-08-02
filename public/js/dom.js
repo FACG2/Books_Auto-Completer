9d5efe2e3579d4e4e2edb053fd11c6f570e3bf26
samer elaila <samer-elaila@live.com>
2017-08-02 16:12:02 +0300

    refactor code

--- public/js/dom.js
+++ public/js/dom.js
@@ -1,4 +1,6 @@
 var input = document.querySelector('input');
+var div = document.querySelector('.form');
+// var books = {};
 
 input.addEventListener('keyup', function(){
   makeRequest(this.value.toLowerCase());
@@ -8,9 +10,26 @@ function makeRequest(str){
   var xhr = new XMLHttpRequest();
   xhr.onreadystatechange = function(){
     if(xhr.readyState == 4 && xhr.status == 200){
-      console.log(xhr.responseText);
+      var data = JSON.parse(xhr.responseText);
+      // books = data;
+      create(data);
     }
   }
   xhr.open('POST' , "/search?word="+str, true);
   xhr.send();
+}
+
+
+function create(obj){
+  var dataList = document.createElement('datalist');
+  dataList.id = "books";
+  dataList.classList.add("books");
+  Object.keys(obj).map(function(index){
+    var option = document.createElement('option');
+    option.value = obj[index];
+    dataList.appendChild(option);
+  });
+  var oldDataList = document.querySelector("#books");
+  div.replaceChild(dataList, oldDataList);
+  // console.log(dataList);
 }
