e64c7f90c85a2939215e6a5ba3fa15f6ed3652ac
Sohad Dader <sohadwd9@gmail.com>
2017-08-02 10:44:03 +0300

    add test

--- src/server.js
+++ src/server.js
@@ -2,6 +2,6 @@ var http = require('http');
 var router = require('./router.js');
 var server = http.createServer(router);
 
-server.listen (process.env.PORT || 4000 , function(){
+server.listen (process.env.PORT || 3000 , function(){
   console.log("You working in port 4000");
 });
